d47cb7d68068d8ef7746b6220785b357f09f7d56
George McIntosh <george@georgecodes.com>
2014-08-23 15:15:55 +0100

    see if cloudbees works

--- src/main/java/com/elevenware/redcorn/visitors/ConstructorArgsInstantiationVisitor.java
+++ src/main/java/com/elevenware/redcorn/visitors/ConstructorArgsInstantiationVisitor.java
@@ -12,4 +12,6 @@ public class ConstructorArgsInstantiationVisitor implements BeanDefinitionVisito
     public void visitAll(Iterable<BeanDefinition> definitions) {
 
     }
+
+    private void pointlessCodeChangeToTestCloudbeesIntegration() {}
 }
