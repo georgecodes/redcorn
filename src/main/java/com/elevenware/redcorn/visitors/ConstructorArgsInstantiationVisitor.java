e8861f58ed2f208614b32169ef7e433389c40118
George McIntosh <george@georgecodes.com>
2014-08-23 15:08:16 +0100

    adding new visitor class

---
+++ src/main/java/com/elevenware/redcorn/visitors/ConstructorArgsInstantiationVisitor.java
@@ -0,0 +1,15 @@
+package com.elevenware.redcorn.visitors;
+
+import com.elevenware.redcorn.beans.BeanDefinition;
+
+public class ConstructorArgsInstantiationVisitor implements BeanDefinitionVisitor {
+    @Override
+    public void visit(BeanDefinition definition) {
+
+    }
+
+    @Override
+    public void visitAll(Iterable<BeanDefinition> definitions) {
+
+    }
+}
